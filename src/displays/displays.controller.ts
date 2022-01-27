67e9a49e3dae3d77eb02526ce5730b160fba2762
Lisuzer <vova-kel@gmail.com>
2022-01-27 12:43:47 +0200

    added descriptions for api responses

--- src/displays/displays.controller.ts
+++ src/displays/displays.controller.ts
@@ -6,14 +6,17 @@ import {
   Put,
   Delete,
   Body,
-  HttpCode,
-  HttpStatus
 } from '@nestjs/common';
 import { CreateDisplayDto } from './dto/create-display.dto';
 import { UpdateDisplayDto } from './dto/update-display.dto';
 import { DisplaysService } from './displays.service';
 import { Display } from './schemas/display.schema';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ParseObjectIdPipe } from 'src/common/objectid.pipe';
 
 @ApiTags('displays')
@@ -22,31 +25,39 @@ export class DisplaysController {
   constructor(private readonly displaysService: DisplaysService) {}
 
   @Get()
-  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ description: 'Displays have been found' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   getAll(): Promise<Display[]> {
     return this.displaysService.getAll();
   }
 
   @Get(':id')
-  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ description: 'Display have been found' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   getOne(@Param('id') id: string): Promise<Display> {
     return this.displaysService.getOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({
+    description: 'Display has been successfully created.',
+    type: Display,
+  })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   create(@Body() createDisplayDto: CreateDisplayDto): Promise<Display> {
     return this.displaysService.create(createDisplayDto);
   }
 
   @Delete(':id')
-  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ description: 'Display have been deleted' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   remove(@Param('id') id: string): Promise<Display> {
     return this.displaysService.remove(id);
   }
 
   @Put(':id')
-  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ description: 'Display have been updated' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   update(
     @Param('id', ParseObjectIdPipe) id: string,
     @Body() updateDisplayDto: UpdateDisplayDto,
--- src/displays/schemas/display.schema.ts
+++ src/displays/schemas/display.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { Document, version } from 'mongoose';
 import { MatrixType } from './matrix-type.enum';
 
@@ -7,15 +8,27 @@ export type DisplayDocument = Display & Document;
 @Schema()
 export class Display {
   @Prop()
+  @ApiProperty({
+    example: 'samsung'
+  })
   mark: string;
 
   @Prop()
+  @ApiProperty({
+    example: 2600,
+  })
   price: number;
 
   @Prop({enum: MatrixType})
+  @ApiProperty({
+    example: MatrixType.TN,
+  })
   matrixType: MatrixType;
 
   @Prop()
+  @ApiProperty({
+    example: 'Curved'
+  })
   model: string;
 }
 
